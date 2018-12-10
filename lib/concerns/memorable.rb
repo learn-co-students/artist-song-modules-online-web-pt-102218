fab7dae265d6b8ea3f1d1c3d9ec727acfe2d0eff
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-10 05:44:10 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+# holds modules that will be used across classes in OO-Ruby
+module Memorable 
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+    
+    def count
+      all.count
+   end
+  end 
+  module InstanceMethods
+    def initialize
+      self.class.all << self 
+    end
+  end 
+end 
